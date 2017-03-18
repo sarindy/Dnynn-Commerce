e9e5719200ef14a10439d76bda3fbb3f81947428
sarindy <sarindyo@yahoo.com>
2017-03-18 13:43:48 +0700

    Project Created

---
+++ src/main/java/dom/dnynn/DnynnApplication.java
@@ -0,0 +1,12 @@
+package dom.dnynn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnynnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnynnApplication.class, args);
+	}
+}
