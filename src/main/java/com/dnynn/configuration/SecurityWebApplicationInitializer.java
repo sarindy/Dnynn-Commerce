179ea0097a30a0d4ed5b0c16483e2d94ea5d37e4
sarindy <sarindyo@yahoo.com>
2017-03-27 16:51:16 +0700

    Complete register user

--- src/main/java/com/dnynn/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.dnynn.configuration;
-
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- src/main/java/com/dnynn/users/UserController.java
+++ src/main/java/com/dnynn/users/UserController.java
@@ -1,5 +1,8 @@
 package com.dnynn.users;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -11,6 +14,7 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,49 +24,53 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private RoleRepository roleRepo;
+
 	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
 	public ModelAndView login() {
 		ModelAndView modelAndView = new ModelAndView();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth != null) {
-			
-			System.out.println(auth.getName());
-		}
 		modelAndView.setViewName("loginPage");
 		return modelAndView;
 	}
-	
-	@RequestMapping(value="/logout", method = RequestMethod.GET)
-    public ModelAndView logout (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){    
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        
-        ModelAndView modelAndView = new ModelAndView();
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+
+		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("loginPage");
 		return modelAndView;
-        
-    }
-	
+
+	}
+
 	@RequestMapping(value = "/user/userHome", method = RequestMethod.GET)
 	public ModelAndView userHome() {
 		ModelAndView modelAndView = new ModelAndView();
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		System.out.println(auth.getName() + "***********" + auth.getAuthorities().toString());
 		User user = userService.findUserByEmail(auth.getName());
+
 		modelAndView.addObject("userName", "Welcome " + user.getFirstName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
 		modelAndView.addObject("adminMessage", "Content Available Only for Users with User Role");
 		modelAndView.setViewName("/user/userHomePage");
 		return modelAndView;
 
 	}
-	
+
 	@RequestMapping(value = "/admin/adminHome", method = RequestMethod.GET)
-	public ModelAndView adminHome() {
+	public ModelAndView adminHome(HttpServletRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println(auth.getName() + " " + auth.getAuthorities().toString());
+		System.out.println(auth.getName() + "***********" + auth.getAuthorities().toString());
+		if (request.isUserInRole("USER")) {
+			System.out.println("YEAH");
+		} else {
+			System.out.println("FUCK");
+		}
 		User user = userService.findUserByEmail(auth.getName());
 		modelAndView.addObject("userName", "Welcome " + user.getFirstName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
 		modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
@@ -71,27 +79,33 @@ public class UserController {
 
 	}
 
-	@RequestMapping(value = "admin/UserRegistration", method = RequestMethod.GET)
+	@RequestMapping(value = "/admin/UserRegistration", method = RequestMethod.GET)
 	public ModelAndView registration() {
 		ModelAndView modelAndView = new ModelAndView();
 		User user = new User();
+		List<Role> roles = new ArrayList<>();
+		roleRepo.findAll().forEach(roles::add);
+
 		modelAndView.addObject("user", user);
+		modelAndView.addObject("roleList", roles);
 		modelAndView.setViewName("/admin/UserRegistrationPage");
 		return modelAndView;
 	}
 
-	@RequestMapping(value = "admin/UserRegistration", method = RequestMethod.POST)
-	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+	@RequestMapping(value = "/admin/UserRegistration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult,@RequestParam(value="role") String role) {
 		ModelAndView modelAndView = new ModelAndView();
 		User userExists = userService.findUserByEmail(user.getEmail());
+		System.out.println(role);
 		if (userExists != null) {
-			bindingResult.rejectValue("email", "error.user",
-					"There is already a user registered with the email provided");
+			bindingResult.rejectValue("email", "error.user", "There is already a user registered with the email provided");
 		}
 		if (bindingResult.hasErrors()) {
 			modelAndView.setViewName("/admin/UserRegistrationPage");
 		} else {
-			userService.saveUser(user);
+			
+			
+			userService.saveUser(user,role);
 			modelAndView.addObject("successMessage", "User has been registered successfully");
 			modelAndView.addObject("user", new User());
 			modelAndView.setViewName("/admin/UserRegistrationPage");
@@ -99,9 +113,5 @@ public class UserController {
 		}
 		return modelAndView;
 	}
-
-
-
-	
 
 }
